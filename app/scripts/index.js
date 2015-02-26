a118989d13a632b15993f67dcbd26f00e3578700
Austin Erlandson <austin@erlandson.com>
2015-02-26 15:35:50 -0600

    working with winGame Logic

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,11 +2,10 @@
 
 var fb = new Firebase('https://tic-tac-toes-app.firebaseio.com'),
   gameArr = [['','',''],['','',''],['','','']],
-  turnChecker = true,
+  currPlayer = player1,
   turnCounter = 0,
   player1 = '/images/tack.jpg',
   player2 = '/images/tick.jpg';
-
 //login register logout features
 
 $('#registerButton').click(function() {
@@ -83,7 +82,7 @@ function sendToFb(data) {
 function clearGame () {
   gameArr = [['','',''],['','',''],['','','']];
   turnCounter =  0;
-  turnChecker = true;
+  currPlayer = player1;
 }
 
 //Create board
@@ -101,15 +100,16 @@ function renderBoard(x) {
   });
   $('table').append($tbody);
 }
-
+var row,
+    col;
 //Click to select move.
 $('#boardWrapper').on('click', 'tbody tr td', function(){
-  var row,
-      col;
   row = this.parentElement.sectionRowIndex;
   col  = this.cellIndex;
   if (gameArr[row][col] === '') {
-    gameArr[row][col] = playerMove();
+    gameArr[row][col] = currPlayer;
+    checkForWin(gameArr);
+    playerTurn();
     renderBoard(gameArr);
     gameOverCheck();
   } else {
@@ -120,25 +120,67 @@ $('#boardWrapper').on('click', 'tbody tr td', function(){
 
 //switch between players and increment turn counter.
 function gameOverCheck () {
-  if (!(turnCounter < 9)) {
+  if (turnCounter < 9) {
+    turnCounter++
+  }
+  else {
     alert('GAME OVER!');
   }
 }
 
 
-function playerMove(){
-	if (turnChecker === true) {
+function checkForWin (x) {
+  debugger;
+  switch(true) {
+    //---------------------
+    //check winning columns
+    //---------------------
+    case (x[0][0] !== '' && (x[0][0] === x[1][0]) === (x[0][0] === x[2][0])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    case ((x[0][1] === x[1][1]) === (x[0][1] === x[2][1])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    case ((x[0][2] === x[1][2]) === (x[0][2] === x[2][2])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    //---------------------
+    //Check winning rows
+    //---------------------
+    case ((x[0][0] === x[0][1]) === (x[0][0] === x[0][2])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    case ((x[1][0] === x[1][1]) === (x[1][0] === x[1][2])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    case ((x[2][0] === x[2][1]) === (x[2][0] === x[2][2])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    //---------------------
+    //Check winning diagonals
+    //---------------------
+    case ((x[0][0] === x[1][1]) === (x[0][0] === x[2][2])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    case ((x[0][2] === x[1][1]) === (x[0][2] === x[2][0])):
+      alert('Player ' + currPlayer + ' Wins!!!');
+      break;
+    default:
+      break;
+  }
+}
 
-		turnChecker = false;
-		turnCounter++;
+
+function playerTurn () {
+	if (currPlayer === player1) {
+
+    currPlayer = player2;
 		return player1;
 
 	} else {
 
-    turnChecker = true;
-    turnCounter++;
+    currPlayer = player1;
     return player2;
 
 	}
 }
-
